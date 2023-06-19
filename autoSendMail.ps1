d3f7d64ceb5bcb8017072cd4287c1c3382bf6f50
Hy Vo <94402902+HyAscension@users.noreply.github.com>
2023-06-19 10:45:00 -0500

    Update autoSendMail.ps1
    Apply a function to keep code in wrap for future use

--- autoSendMail.ps1
+++ autoSendMail.ps1
@@ -6,19 +6,22 @@ $Password = Get-Content ".\mfaGReaderCert.txt" | ConvertTo-SecureString
 $Credential = New-Object System.Management.Automation.PSCredential($AccountName, $Password)
 Connect-MsolService -Credential $Credential
 
-# Get all accounts excluding guest
-$Users = Get-MsolUser -All | Where-Object { $_.UserType -ne "Guest" }
-$Report = [System.Collections.Generic.List[Object]]::new()
-Write-Host "Read" $Users.Count "accounts..."
+# Wrap in function cause redundancy
+function Listing-Departments() {
+    # Get all accounts excluding guest
+    $Users = Get-MsolUser -All | Where-Object { $_.UserType -ne "Guest" }
+    $Report = [System.Collections.Generic.List[Object]]::new()
+    Write-Host "Read" $Users.Count "accounts..."
 
-# Loop through each users to create a custom data line for csv extract
-foreach ($User in $Users) {
-    $ReportLine = [PSCustomObject] @{
-        User        = $User.UserPrincipalName
-        DisplayName = $user.DisplayName
-        Department  = $User.Department
+    # Loop through each users to create a custom data line for csv extract
+    foreach ($User in $Users) {
+        $ReportLine = [PSCustomObject] @{
+            User        = $User.UserPrincipalName
+            DisplayName = $user.DisplayName
+            Department  = $User.Department
+        }
+        $Report.Add($ReportLine)
     }
-    $Report.Add($ReportLine)
+    # $Report | Select User, Department | Sort User | Out-GridView
+    $Report | Sort-Object Name | Export-CSV -NoTypeInformation -Encoding UTF8 # Path\userMailDepartmentList.csv
 }
-# $Report | Select User, Department | Sort User | Out-GridView
-$Report | Sort-Object Name | Export-CSV -NoTypeInformation -Encoding UTF8 # Path\userMailDepartmentList.csv
