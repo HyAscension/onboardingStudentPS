c3d3c1a2cee989af3ff8ac0a1abe8b502d22da53
Hy Vo <94402902+HyAscension@users.noreply.github.com>
2023-06-21 10:41:00 -0500

    Update autoSendMail.ps1
    Import pfx certificate

--- autoSendMail.ps1
+++ autoSendMail.ps1
@@ -23,5 +23,22 @@ function Listing-Departments() {
         $Report.Add($ReportLine)
     }
     # $Report | Select User, Department | Sort User | Out-GridView
-    $Report | Sort-Object Name | Export-CSV -NoTypeInformation -Encoding UTF8 # Path\userMailDepartmentList.csv
+    $Report | Sort-Object Name | Export-CSV -NoTypeInformation -Encoding UTF8 # Path\File.csv
 }
+
+# Import pfx certificate
+$cert = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2
+$secureString = ConvertTo-SecureString "Password" -AsPlainText -Force
+$cert = Import-PfxCertificate -FilePath "Path\File.pfx" -CertStoreLocation 'Cert:\CurrentUser\My' -Password $secureString
+
+# Credential hash table
+$appRegistration = @{
+    TenantId          = ""
+    ClientId          = ""
+    ClientCertificate = $cert
+}
+
+# Get token
+$msalToken = Get-msaltoken @appRegistration -ForceRefresh
+
+$studentList = Import-Csv -Path "Path\File.csv"
